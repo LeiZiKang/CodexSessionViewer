d77a618c6bdcf58ba25d5d1b4ca3f6234a3b9f42
Levi Lei <levilei@esdlife.com>
2025-10-15 18:27:39 +0800

    Initial Commit

---
+++ Codex Session Viewer/Codex_Session_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codex_Session_ViewerApp.swift
+//  Codex Session Viewer
+//
+//  Created by Lei Levi on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Codex_Session_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
